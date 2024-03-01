af83d17e93745cffb92403c068ef725c8acfdf3f
Felipe Duarte <feduarte.dev@gmail.com>
2024-03-01 12:03:44 -0300

    requisito 3

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,5 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    reverse_word = word[::-1]
+    if word != reverse_word or not word:
+        return False
+    return True
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    for _ in word:
+        if low_index == len(word) - 1:
+            return True
+        if word[low_index] == word[high_index]:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return False
