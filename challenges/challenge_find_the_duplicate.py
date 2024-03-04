de8e9e4d517248a74b4ffe8cade8838a6ee690c4
Felipe Duarte <feduarte.dev@gmail.com>
2024-03-04 11:06:04 -0300

    requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,8 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    sorted_nums = sorted(nums)
+    for index, number in enumerate(sorted_nums):
+        if type(number) is not int or number < 0:
+            return False
+        elif index < len(sorted_nums) - 1 and number == sorted_nums[index + 1]:
+            return number
+    return False
--- challenges/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-
-# numbers = [6, 5, 3, 1, 8, 7, 2, 4]
-# merge_sort(numbers, 0, len(numbers))
-# print(numbers)
