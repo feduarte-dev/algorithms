77d8488ba60600e60877e4bf9722e9e9b9e61b85
Felipe Duarte <feduarte.dev@gmail.com>
2024-03-04 10:37:52 -0300

    requisito 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,29 @@
+def bubble_sort(string):
+    n = len(string)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if string[j].lower() > string[j + 1].lower():
+                string[j], string[j + 1] = string[j + 1], string[j]
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_sorted = list(first_string)
+    second_sorted = list(second_string)
+
+    bubble_sort(first_sorted)
+    bubble_sort(second_sorted)
+
+    fixed_first = "".join(first_sorted).lower()
+    fixed_second = "".join(second_sorted).lower()
+
+    if fixed_first != fixed_second or first_string == "":
+        return (
+            fixed_first,
+            fixed_second,
+            False,
+        )
+    return (
+        fixed_first,
+        fixed_second,
+        True,
+    )
--- challenges/tempCodeRunnerFile.py
+++ challenges/tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-(word, 0, len(word) - 1)
+
+
+# numbers = [6, 5, 3, 1, 8, 7, 2, 4]
+# merge_sort(numbers, 0, len(numbers))
+# print(numbers)
